d6e835135f7da9d333ae743c09f4de354bd520c5
Travis Elnicky <travis.elnicky@gmail.com>
2013-07-08 19:27:50 -0600

    added rtc header

--- otolith/nrf51822/Source/rtc/rtc.c
+++ otolith/nrf51822/Source/rtc/rtc.c
@@ -3,11 +3,7 @@
  */ 
 
 #include "nrf.h"
-
-#define LFCLK_FREQUENCY           (32768UL)                 /*!< LFCLK frequency in Hertz, constant */
-#define RTC_FREQUENCY             (8UL)                     /*!< required RTC working clock RTC_FREQUENCY Hertz. Changable */
-#define COMPARE_COUNTERTIME       (3UL)                     /*!< Get Compare event COMPARE_TIME seconds after the counter starts from 0 */
-#define COUNTER_PRESCALER         ((LFCLK_FREQUENCY/RTC_FREQUENCY) - 1)  /* f = LFCLK/(prescaler + 1) */
+#include <rtc.h>
 
 void lfclk_config(void) {
   NRF_CLOCK->LFCLKSRC = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
