77112cd383e6b2056c20d6692db1e5e68c7dec79
Travis Elnicky <travis.elnicky@gmail.com>
2013-07-15 19:48:14 -0600

    fixing broken merge

--- otolith/nrf51822/Include/step_counter.h
+++ otolith/nrf51822/Include/step_counter.h
@@ -3,6 +3,7 @@
 
 #include "acc_driver.h"
 #include "rtc.h"
+#include "app_util.h"
 #define SAMPLE_SIZE 100.0
 #define SAMPLE_RATE 50.0
 #define PI 3.14159265
@@ -35,10 +36,12 @@ typedef struct {
 	uint32_t steps;
 } step_data;
 
-typedef struct {
+struct _step_node {
   step_data data;
-  struct step_node * next;
-} step_node;
+  struct _step_node * next;
+}; 
+
+typedef struct _step_node step_node;
 
 int max_of(int a, int b);
 int min_of(int a, int b);
@@ -51,11 +54,28 @@ int max_axis_offset(int dx, int dy, int dz);
 void get_max_min(measurements *measure, acc_data_t *data, int size);
 int count_steps(measurements *measure, acc_data_t *acc_data_array, int size);
 int count_steps1(measurements *measure, acc_data_t *acc_data_array, int size);
+void push_measurement (step_data data);
+void store_stepCount(int steps);
 
 int fill_data(acc_data_t* acc_array);
 
 uint32_t get_step_count(void);
 
 void step_counter_init(void);
+
+/**@brief Inline function for encoding step_data.
+ *
+ * @param[in]   value            Value to be encoded.
+ * @param[out]  p_encoded_data   Buffer where the encoded data is to be written.
+ *
+ * @return      Number of bytes written.
+ */
+static __INLINE uint8_t step_data_encode(step_data value, uint8_t * p_encoded_data)
+{
+  uint32_encode(value.start_time, p_encoded_data);
+  uint32_encode(value.end_time, p_encoded_data + sizeof(uint32_t));
+  uint32_encode(value.steps, p_encoded_data  + (2 * sizeof(uint32_t)));
+  return (3 * sizeof(uint32_t));
+}
 
 #endif
